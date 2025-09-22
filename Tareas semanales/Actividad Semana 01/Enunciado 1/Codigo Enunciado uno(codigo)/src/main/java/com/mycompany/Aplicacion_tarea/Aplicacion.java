c2a16d682a7107145ad83245584add2da0e450d7
dream987654 <t01293c@ms.upla.edu.pe>
2025-09-21 22:32:47 -0500

    Add files via upload
    Signed-off-by: dream987654 <t01293c@ms.upla.edu.pe>

---
+++ Tareas semanales/Actividad Semana 01/Enunciado 1/Codigo Enunciado uno(codigo)/src/main/java/com/mycompany/Aplicacion_tarea/Aplicacion.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package com.mycompany.tarea;
+import Vista.tarea;
+/**
+ *
+ * @author USER 17
+ */
+public class Tarea_aplicacion {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         tarea tar = new tarea(); 
+    tar.show(); 
+} 
+ 
+    
+}
---
+++ Tareas semanales/Actividad Semana 01/Enunciado 2/Codigo enunciado 2/src/main/java/com/mycompany/enunciado 2/enunciado 2.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tarea_apli_2;
+import Vista.apli; /** * 
+
+@author User 
+
+*/ public class Tarea_apli_2 { 
+
+public static void main(String[] args) { 
+      apli ejem = new apli(); 
+      ejem.show(); 
+} 
+ 
+} 
